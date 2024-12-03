5f349c3fbd3441f134a946dfae1aa105f6db7317
Alex Balderson <alexander.balderson@canonical.com>
2024-12-03 16:33:24 -0700

    local not locals

--- terraform/manifest/outputs.tf
+++ terraform/manifest/outputs.tf
@@ -3,13 +3,13 @@
 
 output "config" {
   value = {
-    app_name    = lookup(locals.yaml_data, "app_name", null)
-    channel     = lookup(locals.yaml_data, "channel". null)
-    config      = lookup(locals.yaml_data, "config", null)
-    constraints = lookup(locals.yaml_data, "constraints", null)
-    resources   = lookup(locals.yaml_data, "resoruces", null)
-    revision    = lookup(locals.yaml_data, "revision", null)
-    series      = lookup(locals.yaml_data, "series", null)
-    units       = lookup(locals.yaml_data, "units", null)
+    app_name    = lookup(local.yaml_data, "app_name", null)
+    channel     = lookup(local.yaml_data, "channel". null)
+    config      = lookup(local.yaml_data, "config", null)
+    constraints = lookup(local.yaml_data, "constraints", null)
+    resources   = lookup(local.yaml_data, "resoruces", null)
+    revision    = lookup(local.yaml_data, "revision", null)
+    series      = lookup(local.yaml_data, "series", null)
+    units       = lookup(local.yaml_data, "units", null)
   }
 }
