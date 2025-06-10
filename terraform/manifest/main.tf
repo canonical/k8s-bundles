2d4895c6361f489ad6a596cb32e52b8fbd6ddca2
Adam Dyess <adam.dyess@canonical.com>
2025-06-09 22:08:55 -0500

    Further simplification of expose decoding

--- terraform/manifest/main.tf
+++ terraform/manifest/main.tf
@@ -19,20 +19,12 @@ locals {
     for app, obj in local.full_map : app => {
       expose = (
         # if the expose data is not present, return null
-        !contains(keys(obj), "expose") || obj.expose == null ? null :
-
-        # if the expose data is utterly empty, return null
-        length([
-          for k in local._allowed_exposed :
-          k if contains(keys(obj.expose), k) && obj.expose[k] != null
-        ]) == 0 ? null :
-
-        # Filter the expose data to only include allowed keys
-        # and ensure that no null values are included
-        {
-          for k, v in obj.expose : k => v
-          if contains(local._allowed_exposed, k) && v != null
-        }
+        !contains(keys(obj), "expose") || obj.expose == null
+          ? null
+          : {
+            for k, v in obj.expose : k => v
+            if contains(local._allowed_exposed, k) && v != null
+          }
       )
     }
   }
