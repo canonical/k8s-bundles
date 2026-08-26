2c6397e96dfd07cc03f52eb412b4895297062276
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2026-08-26 11:38:33 +0400

    fix: cinder_csi org
    Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

--- terraform/openstack/applications.tf
+++ terraform/openstack/applications.tf
@@ -50,7 +50,7 @@ module "openstack_integrator" {
 }
 
 module "cinder_csi" {
-  source      = "git::https://github.com/canonical/cinder-csi-operator//terraform?ref=release_1.35"
+  source      = "git::https://github.com/charmed-kubernetes/cinder-csi-operator//terraform?ref=release_1.35"
 
   model       = var.model
   app_name    = local.cinder_csi_config.app_name
